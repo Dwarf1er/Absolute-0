708a49ec48d050eae23cc161ad6409ee55eb63f6
Dwarf1er <superantou47@gmail.com>
2019-02-10 18:14:48 -0500

    Added basic player movement

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Player : NetworkBehaviour
+{
+    void Update()
+    {
+        if (isLocalPlayer)  //Checks for local player ownership
+        {
+            if (Input.GetKey(KeyCode.A))
+                transform.Translate(Vector3.left);
+
+            if (Input.GetKey(KeyCode.W))
+                transform.Translate(Vector3.forward);
+
+            if (Input.GetKey(KeyCode.S))
+                transform.Translate(Vector3.back);
+
+            if (Input.GetKey(KeyCode.D))
+                transform.Translate(Vector3.right);
+        }
+    }
+}
