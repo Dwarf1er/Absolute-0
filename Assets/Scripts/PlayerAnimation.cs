6f72cc3a49a4834cf31703284023f0551d124037
Sergent-Roubignoles <46965407+Sergent-Roubignoles@users.noreply.github.com>
2019-02-25 10:13:50 -0500

    test player model

---
+++ Assets/Scripts/PlayerAnimation.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimation : MonoBehaviour
+{
+    Animator animator;
+    PlayerUtilities utilities;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        utilities = GetComponent<PlayerUtilities>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //animator.SetFloat("Velocity", utilities.playerVelocity);
+    }
+}
