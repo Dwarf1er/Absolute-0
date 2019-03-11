3e999de15ab37fd0d94e68b501ef45be16777e47
Sergent-Roubignoles <46965407+Sergent-Roubignoles@users.noreply.github.com>
2019-03-11 10:32:20 -0400

    Premier script ennemi

---
+++ Assets/Scripts/Ennemy Scripts/Ennemy.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public abstract class Ennemy : MonoBehaviour
+{
+    //Components
+    Animator Animator { get; set; }
+    NavMeshAgent NavMeshAgent { get; set; }
+
+    //Stats
+    int Tier { get; set; }
+    int MaxHP
+    {
+        get { return hp; }
+        set
+        {
+            hp = value;
+
+            if (hp <= 0) //Trigger NPC death
+            {
+                hp = 0;
+                TriggerDeath();
+            }
+
+            if (hp > MaxHP) //Prevent over-healing
+                hp = MaxHP;
+        }
+    }
+    int HP { get; set; }
+    int Armor { get; set; }
+    float Speed { get; set; }
+    int Damage { get; set; }
+    bool hasRangedAttack { get; set; } //lol ça restera pas là longtemps
+
+    //Backing Store
+    int hp;
+
+    //Functions
+    public abstract void TriggerDeath();
+}
