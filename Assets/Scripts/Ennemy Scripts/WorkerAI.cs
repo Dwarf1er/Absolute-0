faa5158ea8efeea0d3810680a9209cb34ceb3feb
Sergent-Roubignoles <46965407+Sergent-Roubignoles@users.noreply.github.com>
2019-03-13 09:49:50 -0400

    Début de l'animation du worker

---
+++ Assets/Scripts/Ennemy Scripts/WorkerAI.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class WorkerAI : Ennemy
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+        Animator.SetBool("IsStopped", NavMeshAgent.isStopped);
+        Animator.SetFloat("Speed", NavMeshAgent.velocity.magnitude);
+    }
+
+    public override void TriggerDeath()
+    {
+        throw new System.NotImplementedException();
+    }
+}
