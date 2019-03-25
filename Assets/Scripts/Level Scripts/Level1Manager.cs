ed307bda3aee5de59bdf462099dca4cc553ecef6
Sergent-Roubignoles <46965407+Sergent-Roubignoles@users.noreply.github.com>
2019-03-25 19:45:46 -0400

    ennemy networking part 2

--- Assets/Scripts/Level Scripts/Level1Manager.cs
+++ Assets/Scripts/Level Scripts/Level1Manager.cs
@@ -5,18 +5,33 @@ using UnityEngine.Networking;
 
 public class Level1Manager : NetworkBehaviour
 {
-    EnnemyInstantiator Instantiator;
+    [SerializeField] GameObject WorkerPrefab;
+
+    //EnnemyInstantiator Instantiator;
 
     // Start is called before the first frame update
     void Start()
     {
-        Instantiator = GetComponent<EnnemyInstantiator>();
-        Instantiator.CmdSpawnWorker(new Vector3(0, 0, 0), 0);
+        //Instantiator = GetComponent<EnnemyInstantiator>();
+        //Instantiator.CmdSpawnWorker(new Vector3(0, 0, 0), 0);
+    }
+
+    public override void OnStartServer()
+    {
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.G))
+            SpawnWorker();
+    }
+
+    void SpawnWorker()
+    {
+        Vector3 position = Vector3.zero;
+        GameObject newWorker = Instantiate(WorkerPrefab, position, Quaternion.identity) as GameObject;
+        NetworkServer.Spawn(newWorker);
     }
 }
