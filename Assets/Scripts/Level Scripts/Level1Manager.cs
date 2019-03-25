4de160fff98c788fbea4b2f399382881a2151204
Sergent-Roubignoles <46965407+Sergent-Roubignoles@users.noreply.github.com>
2019-03-25 10:21:04 -0400

    Player Networking 2

--- Assets/Scripts/Level Scripts/Level1Manager.cs
+++ Assets/Scripts/Level Scripts/Level1Manager.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
 
-public class Level1Manager : MonoBehaviour
+public class Level1Manager : NetworkBehaviour
 {
     EnnemyInstantiator Instantiator;
 
