4e44ea2453f1ae01a69493fb6623eb92642219bb
Sergent-Roubignoles <46965407+Sergent-Roubignoles@users.noreply.github.com>
2019-03-25 20:16:10 -0400

    networking tests part 2

--- Assets/Scripts/Level Scripts/Level1Manager.cs
+++ Assets/Scripts/Level Scripts/Level1Manager.cs
@@ -24,15 +24,15 @@ public class Level1Manager : NetworkBehaviour
     // Update is called once per frame
     void Update()
     {
-        /*
+        
         if (Input.GetKeyDown(KeyCode.G))
             SpawnWorker();
-        */
+        
     }
 
     void SpawnWorker()
     {
-        Vector3 position = Vector3.zero;
+        Vector3 position = Vector3.forward * 4;
         GameObject newWorker = Instantiate(WorkerPrefab, position, Quaternion.identity) as GameObject;
         NetworkServer.Spawn(newWorker);
     }
