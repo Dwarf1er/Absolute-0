e03e7051b2d332efb9aae7368434c258ffdb09df
Dwarf1er <superantou47@gmail.com>
2019-03-27 09:44:36 -0400

    Added Health Bar graphics in PlayerUI prefab

--- Assets/Scripts/Player Scripts/PlayerStats.cs
+++ Assets/Scripts/Player Scripts/PlayerStats.cs
@@ -7,9 +7,6 @@ public class PlayerStats : NetworkBehaviour
 {
     [SerializeField]
     private int maxHp = 100;
-
-    //This allows the health of the player to be synced with the server
-    [SyncVar]
     int currentHp;
 
     void Awake()
@@ -24,7 +21,6 @@ public class PlayerStats : NetworkBehaviour
 
     public void TakeDamage(int rawDamage)
     {
-        //Synced with server
         currentHp -= rawDamage;
         Debug.Log(transform.name + " now has" + currentHp + " HP");
     }
