a58125ddac4a7bcd7ebfc7b828eb6d5e222b526f
bourret27 <william.m.bourret@gmail.com>
2019-04-03 09:49:09 -0400

    working scene managment

--- Assets/Scripts/MultiplayerMenu.cs
+++ Assets/Scripts/MultiplayerMenu.cs
@@ -52,20 +52,22 @@ public class MultiplayerMenu : NetworkManager
         TxtError = GameObject.Find("TxtError").GetComponent<Text>();
     }
 
-   
 
+    //Méthode pemettant d'instancier le joueur hôte
     public void StartUpHost()
     {
         AssociatePort();
         NetworkManager.singleton.StartHost();
     }
-
+    //Méthode permettant d'instancier le joueur invité
     public void StartUpClient()
     {
         AssociateIPAddress();
-
+        //Nous vérifions s'il y a bel et bien une adresse IP pour faire la connexion
+        //S'il y en a pas, un message d'erreur est lancé
         if (string.IsNullOrEmpty(NetworkManager.singleton.networkAddress))
             TxtError.text = ErrorMessage;
+        //S'il y en a un, la connexion peut être établie
         else
         {
             TxtError.text = string.Empty;
@@ -74,17 +76,17 @@ public class MultiplayerMenu : NetworkManager
         }
         
     }
-
+    //Méthode permettant d'associer l'adresse IP dans la mémoir du Network Manager
     private void AssociateIPAddress()
     {
         NetworkManager.singleton.networkAddress = IPAdress.text;
     }
-
+    //Méthode permettant d'associer un port pour établir la connexion
     private void AssociatePort()
     {
         NetworkManager.singleton.networkPort = 5005;
     }
-
+    //Méthode permettant de faire la transition entre le mode hors ligne et le mode en ligne
     void OnLevelWasLoaded(int level)
     {
         if (level == 0)
@@ -97,7 +99,7 @@ public class MultiplayerMenu : NetworkManager
         }
     }
 
-
+   //Méthode pour instancier tous les gameobjects qui ne sont pas dans la scène du menu multijoueur
     private void SetOtherButtons()
     {
         BtnDisconnect = GameObject.Find("BtnDisconnect").GetComponent<Button>();
@@ -111,19 +113,21 @@ public class MultiplayerMenu : NetworkManager
         BtnClient = GameObject.Find("BtnClient").GetComponent<Button>();
         BtnClient.onClick.AddListener(() => GoToHostScene());
     }
-
+    
+    //Méthode permettant au joueur invité de se rendre à la scène choisit par le joueur hôte
     private void GoToHostScene()
     {
-        if (!string.IsNullOrEmpty(PlayScene))
         NetworkManager.singleton.ServerChangeScene(PlayScene);
     }
 
+    //Méthode permettant de changer vers la scène choisit par le joueur hôte
     private void ChangeScene(string sceneName)
     {
         NetworkManager.singleton.ServerChangeScene(sceneName);
         PlayScene = sceneName;
     }
 
+    //Méthode permettant d'instancier les gameobjects dans le menu multijoueur
     void SetMenuButtons()
     {
         BtnHost = GameObject.Find("BtnHébergerLAN").GetComponent<Button>();
@@ -132,8 +136,10 @@ public class MultiplayerMenu : NetworkManager
         BtnJoin.onClick.AddListener(() => StartUpClient());
     }
 
+    //Méthode permettant d'arrêter la connexion
     private void StopGame()
     {
+        Debug.Log("Bouton cliqué");
         NetworkManager.singleton.StopHost();
     }
 
