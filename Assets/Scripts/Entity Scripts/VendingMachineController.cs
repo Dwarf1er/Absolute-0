fac90f29b6bd5227999393d5e6de34809a527a31
William Bourret <46964762+bourret27@users.noreply.github.com>
2019-04-13 18:26:29 -0400

    Weapons menu pop-up

---
+++ Assets/Scripts/Entity Scripts/VendingMachineController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VendingMachineController : MonoBehaviour
+{
+    [SerializeField]
+    GameObject WeaponsUI;
+    WeaponsStoreController WeaponsStore { get; set; }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        WeaponsStore = WeaponsUI.GetComponent<WeaponsStoreController>();
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Est dedans");
+        WeaponsStore.SetTrigger(true);
+    }
+
+}
---
+++ Assets/Scripts/Player Scripts/WeaponsStoreController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WeaponsStoreController : MonoBehaviour
+{
+    Button BuyBtnM4 { get; set; }
+    Button BuyBtnM4A1 { get; set; }
+    Button BuyBtnM249 { get; set; }
+    Button BuyBtnM110 { get; set; }
+    Button BuyBtnSMAW { get; set; }
+    bool IsTriggered { get; set; }
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.SetActive(false);
+    }
+
+    public void SetTrigger(bool condition)
+    {
+        IsTriggered = condition;
+        Debug.Log("Condition changed");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+            if (IsTriggered)
+            {
+                gameObject.SetActive(true);
+            }
+            
+        
+    }
+}
