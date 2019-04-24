b5d8cdac48ffba63ed2f130d8b8a9e585cc8520c
Sergent-Roubignoles <46965407+Sergent-Roubignoles@users.noreply.github.com>
2019-04-24 09:49:07 -0400

    Weapon damage changed + first functions for ennemy tiers

--- Assets/Scripts/Ennemy Scripts/AssaultGunnerAI.cs
+++ Assets/Scripts/Ennemy Scripts/AssaultGunnerAI.cs
@@ -4,5 +4,8 @@ using UnityEngine;
 
 public class AssaultGunnerAI : Ennemy
 {
+    protected override void SetStats(int ennemyTier)
+    {
 
+    }
 }
--- Assets/Scripts/Ennemy Scripts/WorkerAI.cs
+++ Assets/Scripts/Ennemy Scripts/WorkerAI.cs
@@ -5,5 +5,13 @@ using UnityEngine.AI;
 
 public class WorkerAI : Ennemy
 {
+    int[] HPTiers = { 1, 2, 3, 4 };
+    int[] ArmorTiers = { 1, 2, 3, 4 };
+    int[] SpeedTiers = { 1, 2, 3, 4 };
+    int[] DamageTiers = { 1, 2, 3, 4 };
 
+    protected override void SetStats(int ennemyTier)
+    {
+
+    }
 }
