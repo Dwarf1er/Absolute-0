7e9674bd928b52b35c7cc5b595d8764510ed9c19
Sergent-Roubignoles <46965407+Sergent-Roubignoles@users.noreply.github.com>
2019-04-24 09:05:47 -0400

    Assault Gunner animation added

--- Assets/Scripts/Ennemy Scripts/AssaultGunnerAI.cs
+++ Assets/Scripts/Ennemy Scripts/AssaultGunnerAI.cs
@@ -2,17 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AssaultGunnerAI : MonoBehaviour
+public class AssaultGunnerAI : Ennemy
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
