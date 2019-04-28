a122c90a2f4f90b45acad3ed48f52dad3546b0d8
William Bourret <46964762+bourret27@users.noreply.github.com>
2019-04-28 19:45:38 -0400

    worked on ennemy waves

--- Assets/Scripts/Level Scripts/Level3Manager.cs
+++ Assets/Scripts/Level Scripts/Level3Manager.cs
@@ -6,13 +6,20 @@ using UnityEngine.Networking;
 
 public class Level3Manager : LevelManager
 {
+    [SerializeField]
+    Vector3 SpawnPoint1;
+    [SerializeField]
+    Vector3 SpawnPoint2;
+    [SerializeField]
+    Vector3 SpawnPoint3;
     const float SpawnTimeInterval = 10f;
     const float WaveOneLimit = 6;
     float LastSpawnTime { get; set; }
-    bool IsWaveOne { get; set; }
-    bool IsWaveTwo { get; set; }
+    bool IsWaveOne;
+    bool IsWaveTwo;
     int NumEnnemies { get; set; }
     int IndexSpawn { get; set; }
+    int NumActiveEnnemies { get; set; }
    
     public override void OnStartServer()
     {
@@ -20,12 +27,13 @@ public class Level3Manager : LevelManager
         EnnemySpawnPoints = new List<Vector3>();
         IsWaveOne = true;
         IsWaveTwo = false;
-        EnnemySpawnPoints.Add(new Vector3(7, -26, -40));
-        EnnemySpawnPoints.Add(new Vector3(-62, -28, -28));
-        EnnemySpawnPoints.Add(new Vector3(74, -26, 28));
+        EnnemySpawnPoints.Add(SpawnPoint1);
+        EnnemySpawnPoints.Add(SpawnPoint2);
+        EnnemySpawnPoints.Add(SpawnPoint3);
         LastSpawnTime = 0;
         NumEnnemies = 0;
         IndexSpawn = 0;
+        NumActiveEnnemies = 0;
     }
 
     // Update is called once per frame
@@ -34,7 +42,7 @@ public class Level3Manager : LevelManager
         if (IsWaveOne)
             ExecuteWaveOne();
         if (IsWaveTwo)
-            ExecuteWaveTwo();
+           ExecuteWaveTwo();
         
     }
     void ExecuteWaveOne()
@@ -55,51 +63,83 @@ public class Level3Manager : LevelManager
         }
         else
         {
-            EndWave();
+            EndWave(ref IsWaveOne, ref IsWaveTwo);
         }
+        if (NumEnnemies > 0 && NumEnnemies % 4 == 0)
+        {
+            DestroyEnnemies();
+            NumActiveEnnemies = 0;
+        }
+            
         
-                
-          
-                
     }
 
-    private void EndWave()
+    private void EndWave(ref bool IsCurrentWave, ref bool IsNextWave)
     {
-        if (AreAllDead())
-        {
-            foreach(GameObject g in ActiveEnnemies)
-            {
-                Destroy(g);
-            }
-        }
+        if (AreAllDead()) 
+            IsCurrentWave = false;
+            IsNextWave = true;
+        
     }
 
     private bool AreAllDead()
     {
         bool result = true;
-        foreach(GameObject g in ActiveEnnemies)
-        {
+
+        foreach (GameObject g in ActiveEnnemies)
             if (g != null)
-                if (!g.GetComponent<Ennemy>().isDead)
+                if(!g.GetComponent<Ennemy>().isDead)
                     result = false;
-                
-        }
         return result;
+    }
+
+    private void DestroyEnnemies()
+    {
+        foreach (GameObject g in ActiveEnnemies)
+            if (g != null)
+                if(g.GetComponent<Ennemy>().isDead)
+                    Destroy(g);
     }
 
     void ExecuteWaveTwo()
     {
         Debug.Log("Executing Wave 2");
         LastSpawnTime += Time.deltaTime;
-        if (LastSpawnTime >= SpawnTimeInterval)
+        if (NumEnnemies < 12)
         {
-            if (IndexSpawn == 3)
-                IndexSpawn = 0;
-            SpawnWarrior(EnnemySpawnPoints[IndexSpawn]);
-            NumEnnemies++;
-            IndexSpawn++;
-            LastSpawnTime = 0;
+            if (NumEnnemies < 4)
+            {
+                if (LastSpawnTime >= SpawnTimeInterval)
+                {
+                    if (IndexSpawn == 3)
+                        IndexSpawn = 0;
+                    SpawnWorker(EnnemySpawnPoints[IndexSpawn]);
+                    NumEnnemies++;
+                    IndexSpawn++;
+                    LastSpawnTime = 0;
+
+                }
+            }
+            else
+            {
+                if (IndexSpawn == 3)
+                    IndexSpawn = 0;
+                if (IndexSpawn % 2 == 0)
+                    SpawnWorker(EnnemySpawnPoints[IndexSpawn]);
+                else
+                    SpawnAssaultGunner(EnnemySpawnPoints[IndexSpawn]);
+                    NumEnnemies++;
+                    IndexSpawn++;
+                    LastSpawnTime = 0;
+            }
         }
+        else
+        {
+            //EndWave(IsWaveOne, IsWaveTwo);
+        }
+        if (NumEnnemies > 0 && NumEnnemies % 4 == 0)
+            DestroyEnnemies();
+
     }
 
 
